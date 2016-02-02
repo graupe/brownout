7675bff4a2e5dde07ef036191347337b8a8483cf
Stefan Schulze <shaggadalic+git@gmail.com>
2016-02-02 17:37:24 +0100

    Addd -lm and changed PREFIX to /usr/local

--- config.mk
+++ config.mk
@@ -4,14 +4,14 @@ VERSION = 0.0
 # Customize below to fit your system
 
 # paths
-PREFIX = /usr
+PREFIX = /usr/local
 
 X11INC = /usr/X11R6/include
 X11LIB = /usr/X11R6/lib
 
 # includes and libs
 INCS = -I. -I/usr/include -I${X11INC}
-LIBS = -L/usr/lib -lc -L${X11LIB} -lX11 -lXrandr
+LIBS = -L/usr/lib -lc -lm -L${X11LIB} -lX11 -lXrandr
 
 # flags
 CPPFLAGS = -DVERSION=\"${VERSION}\"
