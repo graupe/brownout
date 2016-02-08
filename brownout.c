e70d28c58d049b1384b460a0336a869c61c665d1
Stefan Schulze <shaggadalic+git@gmail.com>
2016-02-08 18:30:13 +0100

    Some renaming/refactoring

--- brownout.c
+++ brownout.c
@@ -5,18 +5,32 @@
 #include <X11/Xatom.h>
 #include <X11/extensions/Xrandr.h>
 #include <limits.h>
+#include <string.h>
+#include <stdarg.h>
 
 #include "config.h"
 
 char * argv0;
 
-int
-get_brown_percent(Display *dpy, Window root, Atom brown_atom) {
+static void
+die(const char *errstr, ...)
+{
+	va_list ap;
+
+	va_start(ap, errstr);
+	vfprintf(stderr, errstr, ap);
+	va_end(ap);
+	exit(EXIT_FAILURE);
+}
+
+static int
+getintatom(Display *dpy, Window root, Atom brown_atom)
+{
 	int format;
 	unsigned long n = 0, extra = 0;
 	unsigned int *p = NULL;
 	Atom atom;
-	int result = 100;
+	int result = 0;
 
 	if(XGetWindowProperty(dpy, root, brown_atom, 0L, 1L, False, XA_CARDINAL,
 			&atom, &format, &n, &extra, (unsigned char **)&p) == Success && n > 0) {
@@ -26,67 +40,103 @@ get_brown_percent(Display *dpy, Window root, Atom brown_atom) {
 	return result;
 }
 
-void
-set_brown_percent(Display *dpy, Window root, Atom brown_atom, int percent) {
+static void
+setintatom(Display *dpy, Window root, Atom brown_atom, int value)
+{
 	XChangeProperty(dpy, root, brown_atom, XA_CARDINAL, 32,
-			PropModeReplace, (unsigned char*)&percent, 1);
+			PropModeReplace, (unsigned char*)&value, 1);
+}
+
+static void
+brownoutscreen(Display *dpy, int screen, float gr, float gg, float gb)
+{
+	Window root = RootWindow(dpy, screen);
+	XRRScreenResources *res = XRRGetScreenResourcesCurrent(dpy, root);
+
+	for (int c = 0; c < res->ncrtc; c++) {
+		int crtcxid = res->crtcs[c];
+		int size = XRRGetCrtcGammaSize(dpy, crtcxid);
+		XRRCrtcGamma *crtc_gamma = XRRAllocGamma(size);
+
+		for (int i = 0; i < size; i++) {
+			float g = 1.0 * USHRT_MAX * i / (size-1);
+			crtc_gamma->red[i] = g * gr;
+			crtc_gamma->green[i] = g * gg;
+			crtc_gamma->blue[i] = g * gb;
+		}
+		XRRSetCrtcGamma(dpy, crtcxid, crtc_gamma);
+
+		XFree(crtc_gamma);
+	}
+}
+
+static int
+setup(Display *dpy, int mode, int percent)
+{
+	int screen = DefaultScreen(dpy);
+	Window root = RootWindow(dpy, screen);
+	Atom brown = XInternAtom(dpy, "_BROWN_PERCENT", False);
+
+	switch(mode) {
+		case '-': percent = getintatom(dpy, root, brown) - percent; break;
+		case '+': percent = getintatom(dpy, root, brown) + percent; break;
+	}
+	if (percent > 100) percent = 100;
+	else if (percent < 0) percent = 0;
+
+	setintatom(dpy, root, brown, percent);
+	return percent;
+}
+
+static void
+brownallscreens(Display *dpy, int percent)
+{
+	int screen, nscreens = ScreenCount(dpy);
+	float gr = gammar(percent);
+	float gg = gammag(percent);
+	float gb = gammab(percent);
+	for (screen = 0; screen < nscreens; screen++) {
+		brownoutscreen(dpy, screen, gr, gg, gb);
+	}
 }
 
 static void
 usage(void)
 {
-	fprintf(stderr, "usage: %s [{+|-|=}PERCENT] increase,decrease or set the brownout by/to PERCENT\n", argv0);
+	fprintf(stderr, "usage: %s [+|-]PERCENT increase,decrease or set the brownout by/to PERCENT\n", argv0);
 	exit(EXIT_FAILURE);
 }
 
 int
 main(int argc, char **argv)
 {
-	Display *dpy = XOpenDisplay(NULL);
-	int screen = DefaultScreen(dpy);
-	Window root = RootWindow(dpy, screen);
-	Atom brown_atom = XInternAtom(dpy, "_BROWN_PERCENT", False);
-	XRRScreenResources *res = XRRGetScreenResourcesCurrent(dpy, root);
-	int percent = get_brown_percent(dpy, root, brown_atom);
+	char mode = 0;
+	int percent = 0;
+	Display *dpy;
 	argv0 = argv[0];
 
-
-	if (argc <= 1)
+	if (argc < 2)
 		usage(); /* exits */
 
-	 switch(argv[1][0]) {
-		 case '+': percent+= atoi(argv[1]+1);
-			   break;
-		 case '-': percent-= atoi(argv[1]+1);
-			   break;
-		 default:  percent = atoi(argv[1]);
-			   break;
+	if (!strcmp("-v", argv[1]))
+		die("brownout-"VERSION", public domain software\n");
+
+	switch(argv[1][0]) {
+		case '+':
+		case '-': mode = argv[1][0];
+			  argv[1]++;
 	} 
 
-	 if (percent > 100) percent = 100;
-	 else if (percent < 0) percent = 0;
+	percent = atoi(argv[1]);
 
-	 printf("%d\n", percent);
+	if (!(dpy = XOpenDisplay(NULL)))
+		die("Cannot open display\n");
 
-	 set_brown_percent(dpy, root, brown_atom, percent);
+	percent = setup(dpy, mode, percent);
+	printf("%d\n", percent);
+	brownallscreens(dpy, percent);
 
-	 float gr = gammar(percent);
-	 float gg = gammag(percent);
-	 float gb = gammab(percent);
+	XCloseDisplay(dpy);
 
-	 for (int c = 0; c < res->ncrtc; c++) {
-		 int crtcxid = res->crtcs[c];
-		 int size = XRRGetCrtcGammaSize(dpy, crtcxid);
-		 XRRCrtcGamma *crtc_gamma = XRRAllocGamma(size);
-
-		 for (int i = 0; i < size; i++) {
-			 float g = 1.0 * USHRT_MAX * i / (size-1);
-			 crtc_gamma->red[i] = g * gr;
-			 crtc_gamma->green[i] = g * gg;
-			 crtc_gamma->blue[i] = g * gb;
-		 }
-		 XRRSetCrtcGamma(dpy, crtcxid, crtc_gamma);
-
-		 XFree(crtc_gamma);
-	 }
+	return EXIT_SUCCESS;
 }
--- config.def.h
+++ config.def.h
@@ -4,9 +4,9 @@
  * to brain malfunction yellow */
 #include <math.h>
 /* each functions output is used to scale the relevant color channel */
-static float gammar(int percent) { return (.9 + .1*.01*percent); }
-static float gammag(int percent) { return (.4 + .1*.01*percent + .5*exp((.01*percent-1)/4.0)); }
-static float gammab(int percent) { return (.0 + .1*.01*percent + .9*exp( .01*percent-1)); }
+static float gammar(int percent) { return (.9 + .1*(1-.01*percent)); }
+static float gammag(int percent) { return (.3 + .1*(1-.01*percent) + .6*exp((-.01*percent)/3.0)); }
+static float gammab(int percent) { return (.0 + .1*(1-.01*percent) + .9*exp( -.01*percent)); }
 
 #else
 /* This resembles the original way of looking up values from a table.
@@ -14,27 +14,29 @@ static float gammab(int percent) { return (.0 + .1*.01*percent + .9*exp( .01*per
  * one percent */
 struct whitepoint { float r, g, b; };
 static const struct whitepoint whitepoints[] = {
-	{ 1.00,  0.18,  0.00, }, /* 1000K */
-	{ 1.00,  0.42,  0.00, },
-	{ 1.00,  0.54,  0.08, },
-	{ 1.00,  0.64,  0.28, },
-	{ 1.00,  0.71,  0.42, },
-	{ 1.00,  0.77,  0.54, },
-	{ 1.00,  0.82,  0.64, },
-	{ 1.00,  0.86,  0.73, },
-	{ 1.00,  0.90,  0.81, },
-	{ 1.00,  0.93,  0.88, },
-	{ 1.00,  0.97,  0.94, },
-	{ 1.00,  1.00,  1.00, }, /* 6500K */
-	{ 0.95,  0.96,  1.00, },
-	{ 0.91,  0.94,  1.00, },
-	{ 0.87,  0.92,  1.00, },
-	{ 0.85,  0.90,  1.00, },
-	{ 0.82,  0.89,  1.00, },
-	{ 0.80,  0.87,  1.00, },
-	{ 0.78,  0.86,  1.00, }, /* 10000K */
 	{ 0.77,  0.85,  1.00, },
+	{ 0.77,  0.85,  1.00, },
+	{ 0.78,  0.86,  1.00, }, /* 10000K */
+	{ 0.80,  0.87,  1.00, },
+	{ 0.82,  0.89,  1.00, },
+	{ 0.85,  0.90,  1.00, },
+	{ 0.87,  0.92,  1.00, },
+	{ 0.91,  0.94,  1.00, },
+	{ 0.95,  0.96,  1.00, },
+	{ 1.00,  1.00,  1.00, }, /* 6500K */
+	{ 1.00,  0.97,  0.94, },
+	{ 1.00,  0.93,  0.88, },
+	{ 1.00,  0.90,  0.81, },
+	{ 1.00,  0.86,  0.73, },
+	{ 1.00,  0.82,  0.64, },
+	{ 1.00,  0.77,  0.54, },
+	{ 1.00,  0.71,  0.42, },
+	{ 1.00,  0.64,  0.28, },
+	{ 1.00,  0.54,  0.08, },
+	{ 1.00,  0.42,  0.00, },
+	{ 1.00,  0.18,  0.00, }, /* 1000K */
 };
+enum { red, green, blue};
 static float avg(int percent, int color)
 {
 	const int size = -1 + sizeof whitepoints/sizeof whitepoints[0];
@@ -44,13 +46,13 @@ static float avg(int percent, int color)
 	const struct whitepoint * wp = &whitepoints[wpi];
 	float c, cn;
 	switch(color) {
-		case 0: c = wp[0].r, cn = wp[1].r; break;
-		case 1: c = wp[0].g, cn = wp[1].g; break;
-		default: c = wp[0].b, cn = wp[1].b; break;
+		case red:   c = wp[0].r, cn = wp[1].r; break;
+		case green: c = wp[0].g, cn = wp[1].g; break;
+		default:    c = wp[0].b, cn = wp[1].b; break;
 	}
 	return c * (1.0 - avgratio) + cn * avgratio;
 }
-static float gammar(int percent) { return avg(percent, 0); }
-static float gammag(int percent) { return avg(percent, 1); }
-static float gammab(int percent) { return avg(percent, 2); }
+static float gammar(int percent) { return avg(percent, red); }
+static float gammag(int percent) { return avg(percent, green); }
+static float gammab(int percent) { return avg(percent, blue); }
 #endif
